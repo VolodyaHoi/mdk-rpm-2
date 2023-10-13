4f88f1a0edd80a873906d6c6e45aba04b44b7c8b
VolodyaHoi <i4masyrov@gmail.com>
2023-10-13 16:05:24 +0300

    upload

---
+++ LibMas/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LibMas
+{
+    public class libmas
+    {
+        public static void masGenerate(int size, out int[] array2)
+        {
+            int[] array = new int[size];
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 20);
+            }
+            array2 = array;
+        }
+
+        public static void masOpen(String file, out int[] array2)
+        {
+            String line;
+            int value;
+            StreamReader reader = new StreamReader(file);
+            line = reader.ReadLine();
+            int[] array = new int[Convert.ToInt32(line)];
+            for (int i = 0; i < Convert.ToInt32(line); i++)
+            {
+                value = Convert.ToInt32(reader.ReadLine());
+                array[i] = value;
+            }
+            array2 = array;
+
+        }
+
+        public static void masSave(String file, int[] array)
+        {
+            StreamWriter writer = new StreamWriter(file);
+            writer.WriteLine(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                writer.WriteLine(array[i]);
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ Lib_9/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_9
+{
+    public class lib9
+    {
+        public static void production(int[] array, out int product)
+        {
+            int prod = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                prod *= array[i];
+            }
+            product = prod;
+        }
+    }
+}
